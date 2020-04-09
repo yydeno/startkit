0e284aa1783d23de99779b598909da7234fae7eb
xuhongbin <xuhongbin@yy.com>
2020-04-10 02:25:30 +0800

    feat: 增加同构内容

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # deno 用例
 
+## 指令 
++ `deno --allow-net --allow-write http.ts` 启动 http 服务
+
 ## vscode 支持  
 + 安装插件 `axetroy.vscode-deno`    
 + 设置 vscode 配置 settings.json    
--- http.ts
+++ http.ts
@@ -1,9 +1,10 @@
 import { serve } from "https://deno.land/std@v0.40.0/http/server.ts";
-import { logger } from "./logger.ts";
+import { blue, green, red, yellow } from "https://deno.land/std/fmt/colors.ts";
+//import { logger } from "./logger.ts";
 
 const s = serve({ port: 8000 });
-logger.info("http://localhost:8000/");
+console.log(green("http://localhost:8000/"));
 for await (const req of s) {
-  console.log(req.headers)
-  req.respond({ body: "Hello World\n" });
+  console.log(yellow(req.url));
+  req.respond({ body: "Hello World " + Date.now() });
 }
